61df90448283d5540653c8543444a0bb241a866c
Alex Beznos <beznosa@yahoo.com>
2016-01-24 21:00:24 +0200

    add posibility to send multiple images in one request

--- app/assets/javascripts/dropzone_options.js
+++ app/assets/javascripts/dropzone_options.js
@@ -1,19 +1,23 @@
 $(document).ready(function (){
-  //Dropzone.autoDiscover = false ;
+  Dropzone.autoDiscover = false;
 
-  Dropzone.options.newImage = {
-
+  $(".dropzone").dropzone({
     paramName: "image[no_pasted_images]",
+    autoProcessQueue: false,
+    uploadMultiple: true,
+    addRemoveLinks: true,
+    init: function() {
+      var dropzone = this;
 
-    uploadMultiple: true ,
-
-    addRemoveLinks: true ,
-
+      $(".dropzone button").on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        dropzone.processQueue();
+      })
+    },
     success: function(file, response){
       this.removeFile(file);
     }
-
-
-  };
+  });
 
 });
